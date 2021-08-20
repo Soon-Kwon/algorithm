b8876e0bf00dc44c6e4c2be22bb702eb5108248d
Soon-Kwon <koentc010@gmail.com>
2021-08-20 16:34:04 +0900

    210820 짧게

--- 연결리스트/test.py
+++ 연결리스트/test.py
@@ -1,13 +1,26 @@
+
 a = [[1,2,3],4,5]
 b = [[6,7,8],1,2]
 
-print(id(a))
-print(id(b))
-print(id(a[0]))
+a = b.copy()
+a[0] = [11, 22]
 
+print(a)
+print(b)
+
+a = [[1,2,3],4,5]
+b = [[6,7,8],1,2]
 a = b.copy()
 
-print(id(a))
-print(id(b))
-print(id(a[0]))
+a[0][1] = 2
 
+print(a)
+print(b)
+
+import copy
+
+a = copy.deepcopy(b)
+a[0][1] = 4
+
+print(a)
+print(b)
--- 연결리스트/포인터로연결리스트구현.py
+++ 연결리스트/포인터로연결리스트구현.py
@@ -2,8 +2,47 @@ from __future__ import annotations
 from typing import Any, Type    
 
 class Node:
-    # 연결리스트용 노드 클래스
+    # 연결 리스트용 노드 클래스
 
     def __init__(self, data: Any = None, next: Node = None):
         self.data = data
         self.next = next
+
+class LinkedList:
+    # 연결 리스트 클래스
+
+    def __init__(self) -> None:
+        self.no = 0    # 리스트에 등록되어 있는 노드의 개수
+        self.head = None    # 머리 노드에 대한 참조
+        self.current = None     # 현재 주목하고 있는 노드에 대한 참조. 
+    
+    def __len__(self) -> int:
+        # 연결 리스트의 노드 개수 반환
+        return self.no
+
+    def search(self, data: Any) -> int:
+        # data와 값이 같은 노드를 검색
+        cnt = 0
+        ptr = self.head
+        while ptr is not None:
+            if ptr.data == data:
+                self.current = ptr
+                return cnt
+            cnt += 1
+            ptr = ptr.next
+        return -1
+
+    def __contains__(self, data: Any) -> bool:
+        # 연결 리스트에 data가 포함되어 있는지 확인
+        return self.search(data) >= 0
+    
+    def add_first(self, data: Any) -> None:
+        # 맨 앞에 노드를 삽입
+        ptr = self.head # 삽입하기 전의 머리 노드
+        self.head = self.current = Node(data, ptr)
+        self.no += 1
+
+    def add_last(self, data: Any) -> None:
+        # 맨 끝에 노드를 삽입
+        if self.head is None: 
+            self.add_first(data)
