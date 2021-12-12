0e4899c3a0b9ce997e65bf9baa99bb30234a38b6
soon <koentc010@gmail.com>
2021-12-12 15:37:24 +0900

    queue

---
+++ 자료구조/src/queues/ListQueue.java
@@ -0,0 +1,46 @@
+import java.util.NoSuchElementExcepiton;
+public class ListQueue<E> {
+    private Node<E> front, rear;
+    private int size;
+
+    public ListQueue(){
+        front = rear = null;
+        size = 0;
+    }
+
+    public int size(){return size;}
+    public boolean isEmpty(){return size == 0;}
+
+    public void add(E newItem){
+        Node newNode = new Node(newItem,null);
+        if(isEmpty()) front = newNode;
+        else rear.setNext(newNode);
+        rear = newNode;
+        size++;
+    }
+
+    public E remove(){
+        if(isEmpty()) throw new NoSuchElementException();
+        E frontItem = front.getItem();
+        front = front.getNext();
+        if(isEmpty()) rear = null;
+        size--;
+        return frontItem;
+    }
+
+}
+
+class Node<E>{
+    private E item;
+    private Node<E> next;
+
+    public Node(E newItem, Node<E> node){
+        item = newItem;
+        next = node;
+    }
+
+    public E getItem(){return item;}
+    public Node<E> getNext(){return next;}
+    public void setItem(E newItem){item = newItem;}
+    public void setNext(Node<E> newNext){next = newNext;}
+}
